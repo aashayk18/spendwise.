bcad3370715e31af1ce10d761598064185bd5fc3
aashayk18 <aashayk100@gmail.com>
2023-06-27 16:51:29 +0530

    Final functionality updates

--- expenses.js
+++ expenses.js
@@ -2,8 +2,6 @@ document.getElementById('hamburger').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open');
 });
 
-//  localStorage.setItem("accumulatedExcessDeficit",parseFloat(0))
-
 window.addEventListener('DOMContentLoaded', function () {
     var sidebar = document.getElementById('sidebar');
     var windowHeight = window.innerHeight;
@@ -13,8 +11,7 @@ window.addEventListener('DOMContentLoaded', function () {
     const currentYear = new Date().getFullYear();
     document.getElementById('current-year').textContent = currentYear;
     var dailyBudget = this.localStorage.getItem('dailyBudget');
-    var balanceAmount = this.localStorage.getItem("balanceAmount")
-    
+
     var currentDate = new Date();
     var day = String(currentDate.getDate()).padStart(2, '0');
     var month = String(currentDate.getMonth() + 1).padStart(2, '0');
@@ -25,19 +22,41 @@ window.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById('date').textContent = formattedDate;
     document.getElementById('daily-budget-display').textContent = dailyBudget;
-    document.getElementById('excess-deficit').textContent = balanceAmount
+
     this.localStorage.setItem("remainingBudget", dailyBudget)
 
-    // var accumulatedExcessDeficit = this.localStorage.getItem("accumulatedExcessDeficit")
-    // resetAccumulatedExcessDeficit()
-    // var resultMessage = this.localStorage.getItem("result")
-    // document.getElementById('remaining').innerHTML = resultMessage;
+
+    // localforage.setItem("26/06/23", "70").then(function() {
+    //     console.log("Data updated successfully!");
+    // }).catch(function(error) {
+    //     console.error("Error updating data:", error);
+    // });
+    
+    
+    var accumulatedExcessDeficit = 0;
+
+    localforage.iterate(function (value, key, iterationNumber) {
+        var excessDeficit = parseInt(value);
+        if (!isNaN(excessDeficit)) {
+            accumulatedExcessDeficit += excessDeficit;
+        }
+    }).then(function () {
+        document.getElementById('excess-deficit').textContent = accumulatedExcessDeficit;
+        if (accumulatedExcessDeficit < 0) {
+            document.getElementById("excess-deficit").classList.add('deficit');
+            document.getElementById("excess-deficit").classList.remove('excess');
+        } else {
+            document.getElementById("excess-deficit").classList.add('excess');
+            document.getElementById("excess-deficit").classList.remove('deficit');
+        }
+    }).catch(function (error) {
+        console.error('Error calculating accumulated excess/deficit:', error);
+    });
 });
 
 
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
-    // sidebar.classList.toggle('sidebar--active');
 }
 
 document.getElementById('expense-form').addEventListener('submit', function (event) {
@@ -55,58 +74,54 @@ document.getElementById('expense-form').addEventListener('submit', function (eve
     var remainingBudget = parseInt(localStorage.getItem("remainingBudget"));
     remainingBudget = remainingBudget - expenseAmount;
     localStorage.setItem("remainingBudget", remainingBudget);
-    document.getElementById("excess-deficit").innerHTML = remainingBudget;
   
-    if (remainingBudget < 0) {
-        document.getElementById("excess-deficit").classList.add('deficit');
-        document.getElementById("excess-deficit").classList.remove('excess');
-    } else {
-        document.getElementById("excess-deficit").classList.add('excess');
-        document.getElementById("excess-deficit").classList.remove('deficit');
-    }
-
-
-    // Retrieve the date, excess/deficit, and other relevant data from the input fields
     var dateInput = formattedDate;
     var dateParts = dateInput.split('/');
     var year = dateParts[2];
     var month = String(dateParts[1]).padStart(2, '0');
     var day = String(dateParts[0]).padStart(2, '0');
-
+  
     var date = day + '/' + month + '/' + year;
     var excessDeficit = remainingBudget;
   
     localforage.setItem(date, excessDeficit)
       .then(function () {
         document.getElementById('expense-form').reset();
-        console.log(date)
-        console.log(excessDeficit)
+        console.log(date);
+        console.log(excessDeficit);
         console.log('Data stored successfully!');
+  
+        // Update accumulatedExcessDeficit
+        var accumulatedExcessDeficit = 0;
+        localforage.iterate(function (value) {
+          var excessDeficit = parseInt(value);
+          if (!isNaN(excessDeficit)) {
+            accumulatedExcessDeficit += excessDeficit;
+          }
+        }).then(function () {
+          document.getElementById('excess-deficit').textContent = accumulatedExcessDeficit;
+          if (accumulatedExcessDeficit < 0) {
+            document.getElementById("excess-deficit").classList.add('deficit');
+            document.getElementById("excess-deficit").classList.remove('excess');
+          } else {
+            document.getElementById("excess-deficit").classList.add('excess');
+            document.getElementById("excess-deficit").classList.remove('deficit');
+          }
+        }).catch(function (error) {
+          console.error('Error calculating accumulated excess/deficit:', error);
+        });
+  
       })
       .catch(function (error) {
         console.error('Error storing data:', error);
       });
-
-     // var dailyBudget = parseInt(localStorage.getItem('dailyBudget'));
-    // var balanceAmount = this.localStorage.getItem("balanceAmount")
-    // document.getElementById('excess-deficit').textContent = balanceAmount
-
-    // var excessDeficitField = document.getElementById('excess-deficit');
-
-    // var accumulatedExcessDeficit = parseFloat(excessDeficitField.textContent) + remainingBudget;
-
-    // excessDeficitField.textContent = accumulatedExcessDeficit;
-    // localStorage.setItem("accumulatedExcessDeficit", parseFloat(accumulatedExcessDeficit))
-
-    // console.log(accumulatedExcessDeficit)
-    // var surplusOrDeficit = remainingBudget >= 0 ? "surplus" : "deficit";
-    // var surplusOrDeficitAmount = Math.abs(remainingBudget);
-
-    // var resultMessage = "You have " + surplusOrDeficit + " of Rs. " + surplusOrDeficitAmount + " for " + formattedDate;
-    // localStorage.setItem("result",resultMessage)
-
-    //    document.getElementById('remaining').innerHTML = resultMessage;
-});
+  
+    var excessOrDeficit = remainingBudget >= 0 ? "excess" : "deficit";
+    var resultMessage = "You have " + excessOrDeficit + " of Rs. " + excessDeficit + " for " + date;
+    localStorage.setItem("result", resultMessage)
+    document.getElementById('remaining').innerHTML = resultMessage;
+  });
+  
 
 function resetAccumulatedExcessDeficit() {
     localStorage.setItem("accumulatedExcessDeficit", 0)
--- index.js
+++ index.js
@@ -12,45 +12,14 @@ window.addEventListener('DOMContentLoaded', function () {
 
 });
 
-// document.getElementById('date-form').addEventListener('submit', function (event) {
-//   event.preventDefault();
-
-//   // Retrieve the date and text data from the input fields
-//   var dateInput = document.getElementById('date-input').value;
-//   var textData = document.getElementById('text-input').value;
-
-//   // Convert the date from dd/mm/yyyy to yyyy-mm-dd format
-//   var dateParts = dateInput.split('-');
-//   var year = dateParts[0];
-//   var month = String(dateParts[1]).padStart(2, '0');
-//   var day = String(dateParts[2]).padStart(2, '0');
-//   var date = day + '/' + month + '/' + year;
-
-//   // Store the date and text data in localForage
-//   localforage.setItem(date, textData)
-//     .then(function () {
-//       // Reset the form fields
-//       document.getElementById('date-form').reset();
-//       console.log('Data stored successfully!');
-//       console.log(date);
-//       console.log(textData);
-//     })
-//     .catch(function (error) {
-//       console.error('Error storing data:', error);
-//     });
-// });
-
-
 function resetAll(){
   localStorage.setItem("remainingBudget", 0)
   localStorage.setItem("dailyBudget", 0)
   localStorage.setItem("balanceAmount", 0)
-  // localStorage.setItem("accumulatedExcessDeficit", 0)
 }
 
 function toggleSidebar() {
   const sidebar = document.getElementById('sidebar');
-  // sidebar.classList.toggle('sidebar--active');
 }
 
 function resetAccumulatedExcessDeficit() {
